a19bb2ce74db8b831565b2a72704b780eca6898b
MaithiliPawde <maithilipawde07@gmail.com>
2024-03-11 11:53:04 +0530

    day2 done

---
+++ golang/day02.go
@@ -0,0 +1,69 @@
+// 	// var (
+// 	// 	marks1     int
+// 	// 	marks2     int
+// 	// 	marks3     int
+// 	// 	roll       int
+// 	// 	percentage int
+// 	// 	sum        int
+// 	// 	name       string
+// 	// 	branch     string
+// 	// )
+
+// 	// fmt.Printf("\n**Please Enter The Details**\n")
+// 	// fmt.Println("**Please Enter Your Name,Branch,Id**")
+// 	// fmt.Scan(&name, &branch, &roll)
+// 	// fmt.Println("**Please Enter Your marks1 marks2 marks 3**")
+// 	// fmt.Scan(&marks1, &marks2, &marks3)
+// 	// sum = marks1 + marks2 + marks3
+// 	// percentage = (sum * 100) / 300
+// 	// print("\n\n")
+// 	// println("**SHRI RAMDEOBABA COLLEGE OF ENGINEERING AND MANAGEMENT**")
+// 	// print("                Student Grade Card")
+// 	// fmt.Printf("\nName : %s\tId:'%d'\n", name, roll)
+// 	// fmt.Printf("Branch:'%s'\tscore:'%d/300'\n", branch, sum)
+// 	// println("DBMS:", marks1)
+// 	// println("OS:", marks2)
+// 	// println("DHLV:", marks3)
+// 	// fmt.Printf("Percentage:%d%%", percentage)
+// 	// if percentage > 40 {
+// 	// 	print("\nCongulations You Have Passed the Exam !!")
+// 	// } else {
+// 	// 	print("\nYou have Failed ,Better Luck Next Time :) ")
+// 	// }
+////////////////////////////////////////////////////////////////////////////////
+// 	// integer := 23
+// 	// fmt.Printf("%T %T\n", integer, &integer)
+// 	// fmt.Print(integer, &integer)
+
+// 	// number := 1234.575883939
+// 	// fmt.Printf("number:%5.2f", number)
+
+// 	// fmt.Printf("%*s\n", 40, "text")//width of output is 40  can be specified "%040s"
+
+// 	// ch := 'A'
+// 	// bch := "ketan"
+// 	// fmt.Printf("%T %T\n", ch, bch)
+
+// 	// var val byte = 'A' //character define kar sakte hai with the help of
+// 	// fmt.Printf("Character value:%c", val)
+// 	// fmt.Printf("\nASCII value :%d", val)
+// 	// var val float64=-19.25
+// 	// fmt.Printf("Absolute value of %f is %f",val,math.Abs(val))//Abs value takes float input only
+// 	// var num1 float64 = 10.34
+// 	// var num2 float64 = 2.7
+// 	// var large float64 = 0
+// 	// large = math.Max(float64(num1), float64(num2)) //type conversion
+// 	// fmt.Printf("Largest number is:%f", large)
+// 	// large = math.Min(float64(num1), float64(num2)) //type conversion
+// 	// fmt.Printf("\nSmallest number is:%f", large)
+// 	// large = math.Pow(num1, num2) //type conversion
+// 	// fmt.Printf("\nExponent number is:%g", large)
+// 	// large = math.Sqrt(float64(num1)) //type conversion
+// 	// fmt.Printf("\nSqure Root number is:%f", large)
+// 	// large = math.Ceil(float64(num1)) //type conversion
+// 	// fmt.Printf("\nCeil Root number is:%f", large)
+// 	// large = math.Floor(float64(num1)) //type conversion
+// 	// fmt.Printf("\nFoor Root number is:%f\n", large)
+
+// 	// print("Using println instead of fmt.Println  ")
+// 	// print("Using println instead of fmt.Println")
